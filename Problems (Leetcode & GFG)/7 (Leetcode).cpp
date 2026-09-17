10d0d9d11b04c360e356db1dd5f8ae1bb2d5ea12
Rajdeep Beniwal <rajdeepbeniwal96@gmail.com>
2026-09-17 22:37:31 +0530

    Add solution to reverse an integer with overflow handling

---
+++ Problems (Leetcode & GFG)/7 (Leetcode).cpp
@@ -0,0 +1,16 @@
+#include <climits>
+class Solution {
+public:
+    int reverse(int x) {
+        
+        long long int ans=0,rem;
+        while(x!=0){
+            rem=x%10;
+            x/=10;
+            if(ans>INT_MAX/10 || ans<INT_MIN/10) // Check for overflow before updating ans
+            return 0;
+            ans=ans*10+rem;
+        }
+        return ans;
+    }
+};
