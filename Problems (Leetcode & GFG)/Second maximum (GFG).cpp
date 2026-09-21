70daaf4c1747289029637be8420877b9c28e2b25
Rajdeep Beniwal <rajdeepbeniwal96@gmail.com>
2026-09-21 22:37:42 +0530

    Code to find the second maximum element in an array

---
+++ Problems (Leetcode & GFG)/Second maximum (GFG).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int firstMax = arr[0];
+    int secondMax = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > firstMax) {
+            secondMax = firstMax;
+            firstMax = arr[i];
+        } else if (arr[i] > secondMax && arr[i] != firstMax) {
+            secondMax = arr[i];
+        }
+    }
+
+    cout << "Second maximum element is: " << secondMax << endl;
+
+    return 0;
+}
