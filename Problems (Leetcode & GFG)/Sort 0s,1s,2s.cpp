152a9c119afccad3cd4115027274170d8b192527
Rajdeep Beniwal <rajdeepbeniwal96@gmail.com>
2026-09-23 23:02:38 +0530

    Code to sort 0s,1s,2s

---
+++ Problems (Leetcode & GFG)/Sort 0s,1s,2s.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        int low =0;
+        int high = arr.size()-1;
+        int mid = 0;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr[mid],arr[low]);
+                mid++,low++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+             else{
+                swap(arr[mid],arr[high]);
+                high--;
+            }
+        }
+    }
+};
